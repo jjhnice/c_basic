253d0b687f150e57096edb45988f58b7a3bdd593
jjhnice <112914420+jjhnice@users.noreply.github.com>
2022-09-19 10:48:32 +0900

    finished chapter01

---
+++ chapter02/c_printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// 제어 문자
+//  + \n : 한줄 개행
+//  + \t : 탭(8칸 스페이스)
+int main(void)
+{
+    /* code */
+    printf("HELLO\n");
+    printf("JUNHYEOCK \t JUNG");
+
+    return 0;
+}
