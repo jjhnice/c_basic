66a57aa94fd3d131e8b1a57c2c78ef8f1c9e1f39
jjhnice <112914420+jjhnice@users.noreply.github.com>
2022-10-05 10:34:15 +0900

    Add files via upload

---
+++ chapter04/c_operator_logical.c
@@ -0,0 +1,20 @@
+/**
+ *  논리연산자
+ *  1.AND(&&)
+ * 
+ *  2.OR(||)
+*/
+#include  <stdio.h>
+
+int main(void) {
+    int a = 30;
+    int res;
+
+    res = (a > 10)  &&  (a < 20);
+    printf("(a > 10 ) && (a < 20)  :  %d\n", res);
+    res = (a < 10)  ||  (a > 20);
+    printf("(a < 10 ) || (a > 20)  :  %d\n", res);
+    res = !(a >= 30);
+    printf("!(a >= 30 ) : %d\n", res);
+    return 0;
+}
